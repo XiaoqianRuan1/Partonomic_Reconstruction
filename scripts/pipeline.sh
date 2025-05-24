a9045c34440c1f0e1fd8767ac51b15c1c022365b
XiaoqianRuan1 <156952688+XiaoqianRuan1@users.noreply.github.com>
2025-05-24 13:41:09 -0500

    Add files via upload

---
+++ scripts/pipeline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+if ! [[ ${tag:0:1} =~ ^[0-9] ]]
+    then 
+        CURRENTDATE=`date +"%m%d"`
+        run_tag="${CURRENTDATE}_$tag"
+else
+    run_tag=$tag
+fi
+
+CUDA_VISIBLE_DEVICES=$cuda python src/trainer.py --tag $run_tag --config $config
+#CUDA_VISIBLE_DEVICES=$cuda python src/train_test.py --tag $run_tag --config $config
+#CUDA_VISIBLE_DEVICES=$cuda python src/part_trainer.py --tag $run_tag --config $config
